405f9847f28b0456aeeebb506a9e2d191ca6e302
Suvojit Ghosh <suvo.ghosh.965@gmail.com>
2025-07-06 16:17:51 +0530

    Setup initial backend service

---
+++ app/dupligone-backend/src/main/java/org/projects/dupligonebackend/DupligoneBackendApplication.java
@@ -0,0 +1,13 @@
+package org.projects.dupligonebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DupligoneBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DupligoneBackendApplication.class, args);
+    }
+
+}
---
+++ app/dupligone-backend/src/test/java/org/projects/dupligonebackend/DupligoneBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.projects.dupligonebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DupligoneBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
