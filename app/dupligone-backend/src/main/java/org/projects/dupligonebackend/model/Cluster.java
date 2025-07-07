475166e7d5c12021cfcdf8acec614544c8708268
Suvojit Ghosh <suvo.ghosh.965@gmail.com>
2025-07-07 15:52:33 +0530

    Add model layer and setup db connection

---
+++ app/dupligone-backend/src/main/java/org/projects/dupligonebackend/model/Cluster.java
@@ -0,0 +1,31 @@
+package org.projects.dupligonebackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Cluster {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String tag;
+    private Instant createdAt;
+    private boolean finalized;
+
+    @OneToMany(mappedBy = "cluster", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Photo> photos;
+
+}
---
+++ app/dupligone-backend/src/main/java/org/projects/dupligonebackend/model/Photo.java
@@ -0,0 +1,40 @@
+package org.projects.dupligonebackend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Photo {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String filePath;
+
+    @Column(length = 128)
+    private String hash;
+
+    private Integer brightness;
+    private Integer sharpness;
+    private Integer faceCount;
+
+    private boolean isBest;
+
+    private Instant analyzedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cluster_id")
+    private Cluster cluster;
+}
