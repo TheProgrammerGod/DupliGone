c8c54960dc56e82a22da0e9b25bc6635ab43b77d
Suvojit Ghosh <suvo.ghosh.965@gmail.com>
2025-07-08 13:53:40 +0530

    Add Repository layer

---
+++ app/dupligone-backend/src/main/java/org/projects/dupligonebackend/repository/ClusterRepository.java
@@ -0,0 +1,10 @@
+package org.projects.dupligonebackend.repository;
+
+import org.projects.dupligonebackend.model.Cluster;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface ClusterRepository extends JpaRepository<Cluster, UUID> {
+
+}
---
+++ app/dupligone-backend/src/main/java/org/projects/dupligonebackend/repository/PhotoRepository.java
@@ -0,0 +1,19 @@
+package org.projects.dupligonebackend.repository;
+
+import org.projects.dupligonebackend.model.Photo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface PhotoRepository extends JpaRepository<Photo, UUID> {
+
+    public List<Photo> findByClusterId(UUID clusterId);
+
+    public List<Photo> findByIsBestTrue();
+
+    public List<Photo> findByHash(String hash);
+
+    public boolean existsByHash(String hash);
+
+}
