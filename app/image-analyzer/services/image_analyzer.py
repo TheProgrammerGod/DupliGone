bcb91814ca169e70318fe93b99ce3c74113610e8
Suvojit Ghosh <suvo.ghosh.965@gmail.com>
2025-07-20 14:53:44 +0530

    Add face detection and smile score

--- app/image-analyzer/services/image_analyzer.py
+++ app/image-analyzer/services/image_analyzer.py
@@ -4,6 +4,10 @@ import cv2
 import numpy as np
 from pathlib import Path
 
+#Load Haar Cascades
+_face_cascade = cv2.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
+_smile_cascade = cv2.CascadeClassifier(cv.data.haarcascades + 'haarcascade_smile.xml')
+
 def cv2_to_pil(cv_image: np.ndarray) -> Image.Image:
     """Convert a cv2 image to a PIL Image."""
     color_converted = cv2.cvtColor(cv_image, cv2.COLOR_BGR2RGB)
@@ -39,3 +43,24 @@ def calculate_contrast(image : np.ndarray) -> float:
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Convert to grayscale
     contrast = gray.std() # Standard deviation of pixel intensities
     return contrast
+
+def calculate_face_metrics(image : np.ndarray) -> dict:
+    """Detect faces and smiles in the image."""
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY) # Convert to grayscale
+    faces = _face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
+    
+    smile_count = 0
+    for (x, y, w, h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        smiles = _smile_cascade.detectMultiScale(roi_gray, scaleFactor=1.7, minNeighbors=22)
+        if(smiles > 0):
+            smile_count += 1
+    
+    face_count = len(faces)
+    smile_score = smile_count / face_count if face_count > 0 else 0.0
+    
+    return {
+        "face_count": face_count,
+        "smile_score": round(smile_score, 2)  # Round to 2 decimal places
+    }
+    
