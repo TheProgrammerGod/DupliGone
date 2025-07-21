cd2099e54e97ddb5029de8a7ee33fa561c3a68d1
Suvojit Ghosh <suvo.ghosh.965@gmail.com>
2025-07-21 23:18:44 +0530

    Add DB Config

--- app/image-analyzer/config/settings.py
+++ app/image-analyzer/config/settings.py
@@ -11,3 +11,10 @@ class RabbitMQConfig:
     USER = os.environ.get('RABBITMQ_USER')
     PASSWORD = os.environ.get('RABBITMQ_PASS')
     QUEUE_NAME = os.environ.get('PHOTO_UPLOADED_QUEUE', 'photo.uploaded.queue')
+    
+class DBConfig:
+    HOST = os.environ.get('POSTGRES_HOST', 'localhost')
+    PORT = os.environ.get('POSTGRES_PORT', '5432')
+    USER = os.environ.get('POSTGRES_USER')
+    PASSWORD = os.environ.get('POSTGRES_PASSWORD')
+    DATABASE = os.environ.get('POSTGRES_DB')
