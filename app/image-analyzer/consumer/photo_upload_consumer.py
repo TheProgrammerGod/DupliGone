5c906467af9814515be984f7135035101e7b4b49
Suvojit Ghosh <suvo.ghosh.965@gmail.com>
2025-07-26 15:20:24 +0530

    Add exponential backoff for photo retry

--- app/image-analyzer/consumer/photo_upload_consumer.py
+++ app/image-analyzer/consumer/photo_upload_consumer.py
@@ -7,6 +7,10 @@ from services.image_loader import load_image
 from services.image_analyzer import analyze_image
 from services.db_service import update_photo_metrics
 
+# Constants
+MAX_RETRIES = 3
+BASE_DELAY_MS = 2000 # 2 seconds base
+
     
 def callback(ch, method, properties, body):
     try:
@@ -24,8 +28,36 @@ def callback(ch, method, properties, body):
         print(f"[INFO] Saved metrics to DB for photo ID: {message.photo_id}")
         ch.basic_ack(delivery_tag=method.delivery_tag)
     except Exception as e:
-        print(f"[ERROR] Failed to process message: {e}")
-        ch.basic_nack(delivery_tag=method.delivery_tag, requeue=True)
+        # Extract retry count from headers or message properties
+        retry_count = body.get('retry_count', 0)
+        
+        if retry_count < MAX_RETRIES:
+            # Exponential backoff delay
+            delay_ms = BASE_DELAY_MS * (2 ** retry_count)
+            
+            print(f"[WARN] Processing failed, retrying (attempt {retry_count + 1}) after {delay_ms}ms : {e}")
+            
+            # Re-publish the message with increased retry count and delay
+            retry_message = {
+                **body,
+                'retry_count': retry_count + 1
+            }
+            
+            prop = pika.BasicProperties(
+                expiration=str(delay_ms),  # Set message TTL (in milliseconds)
+                content_type='application/json'
+            )
+            
+            ch.basic_publish(
+                exchange='',
+                routing_key=RabbitMQConfig.QUEUE_NAME,
+                body=json.dumps(retry_message),
+                properties=prop
+            )
+        else:
+            print(f"[ERROR] Max retries reached for photoId {body.get('photo_id')}. Discarding message: {e}")
+        ch.basic_ack(delivery_tag=method.delivery_tag)
+        
 
 def start_consumer():
     #Load RabbitMQ configuration from environment variables
